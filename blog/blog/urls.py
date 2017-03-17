8d0b44a9a6f5819df6398064604aee0b866541bd
Seul Chan Kim <seulchankim@Seului-MacBook-Pro.local>
2017-03-18 02:22:36 +0900

    add rooms view

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from django.contrib import admin
-from .views import home
+from .views import home, room
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', home),
+    url(r'^rooms/(?P<room_id>\d+)/$', room),
 ]
--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,4 +1,10 @@
 from django.http.response import HttpResponse
+import requests
 
 def home(request):
     return HttpResponse('Hello world')
+
+def room(request, room_id):
+    url = "https://api.zigbang.com/v1/items?detail=true&item_ids=" + room_id
+    response = requests.get(url)
+    return HttpResponse(response.content)
