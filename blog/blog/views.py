7b027db4f49b8a1c0e6cbdc6f2d140f20f8f6e34
Seul Chan Kim <seulchankim@Seului-MacBook-Pro.local>
2017-03-18 19:09:25 +0900

    Update home: add watch list

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,10 +1,13 @@
 from django.http.response import HttpResponse
 import requests
 
-def home(request):
-    return HttpResponse('Hello world')
 
 def room(request, room_id):
     url = "https://api.zigbang.com/v1/items?detail=true&item_ids=" + room_id
+    response = requests.get(url)
+    return HttpResponse(response.content)
+
+def home(request):
+    url = "https://watcha.net/home/news.json?page=1&per=50"
     response = requests.get(url)
     return HttpResponse(response.content)
